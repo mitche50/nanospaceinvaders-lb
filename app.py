9699fd9c2fa22e55f6b38e288696f030fc0830ae
mitche50 <mitche50@gmail.com>
2019-06-17 13:55:16 -0400

    add db support

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+import sys
+import json
+from flask_heroku import Heroku
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+heroku = Heroku(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://uckuuqvvpuravr:8ed73c5895c2e4ad35a443da461605e85233994efc09b37f6abc78cbb7287f17@ec2-54-235-104-136.compute-1.amazonaws.com:5432/daad1ji37o07ln'
+db = SQLAlchemy(app)
+
+
+class Leaderboard(db.Model):
+    __tablename__ = "leaderboard"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text())
+    score = db.Column(db.Integer)
+
+    def __init__(self, username):
+        self.username = username
+        self.score = 0
 
 
 @app.route("/")
